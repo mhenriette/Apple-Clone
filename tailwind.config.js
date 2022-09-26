de77abce86e20f10e2264c61e4cb3822b577d4bb
Henriette <mhenriette00@gmail.com>
2022-09-26 18:18:31 +0200

    Added my project

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            colors: {
+                blue: '#2997ff',
+                myblack: '#040505',
+
+            }
+
+        },
+    },
+    plugins: [],
+}
